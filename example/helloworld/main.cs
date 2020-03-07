9ed3a5a717b7d161b00dd009b772f724188b6316
Hajime Hoshi <hajimehoshi@gmail.com>
2020-03-08 00:22:04 +0900

    Add main.go

---
+++ example/helloworld/main.cs
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: Apache-2.0
+
+using System;
+using Go2DotNet.AutoGen.github_2ecom.hajimehoshi.go2dotnet.example.helloworld;
+
+namespace Go2DotNet.Example.HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---- start ----");
+            Go go = new Go();
+            go.Run(args);
+        }
+    }    
+}
