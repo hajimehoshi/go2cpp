85281ea86d995b9b3db721136d5b2b84ab07b67c
Hajime Hoshi <hajimehoshi@gmail.com>
2020-03-08 15:07:53 +0900

    Rename generated file names

--- example/helloworld/run.sh
+++ example/helloworld/run.sh
@@ -1,3 +1,3 @@
 env GOOS=js GOARCH=wasm go build -tags example -o helloworld.wasm .
-go run ../../ -wasm helloworld.wasm -namespace Go2DotNet.Example.HelloWorld.AutoGen > go.cs
+go run ../../ -wasm helloworld.wasm -namespace Go2DotNet.Example.HelloWorld.AutoGen > gen.cs
 dotnet run .
