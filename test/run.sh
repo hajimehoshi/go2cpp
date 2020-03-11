973c6f7956c5b1a3f0a560820c3ca507ccbe178c
Hajime Hoshi <hajimehoshi@gmail.com>
2020-03-12 02:51:18 +0900

    Update tests

--- test/run.sh
+++ test/run.sh
@@ -1,6 +1,6 @@
 for pkg in fmt testing; do
   echo "# Test $pkg"
   env GOOS=js GOARCH=wasm go test -c -o test.wasm -trimpath $pkg
-  go run .. -wasm test.wasm -namespace Go2DotNet.Test.AutoGen -profile > gen.cs
-  dotnet run . -- -v
+  go run .. -wasm test.wasm -namespace Go2DotNet.Test.AutoGen > gen.cs
+  dotnet run .
 done
