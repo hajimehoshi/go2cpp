69730aa8d1efb05a60503b12fa076b3ed7ae9320
Hajime Hoshi <hajimehoshi@gmail.com>
2020-03-13 01:11:32 +0900

    test: Accept any arguments

--- test/run.sh
+++ test/run.sh
@@ -1,4 +1,5 @@
 echo "# Test $1"
 env GOOS=js GOARCH=wasm go test -c -o test.wasm -trimpath $1
 go run .. -wasm test.wasm -namespace Go2DotNet.Test.AutoGen > gen.cs
-dotnet run . -- -test.v
+shift
+dotnet run . -- $*
