2c51b41717ff679dcb97376238a03e5233ec6a97
Hajime Hoshi <hajimehoshi@gmail.com>
2020-03-14 20:20:39 +0900

    example/goroutine: Rename

--- example/goroutine/main.cs
+++ example/goroutine/main.cs
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 using System;
-using Go2DotNet.Example.Goroutines.AutoGen;
+using Go2DotNet.Example.Goroutine.AutoGen;
 
-namespace Go2DotNet.Example.Goroutines
+namespace Go2DotNet.Example.Goroutine
 {
     class Program
     {
--- example/goroutines/run.sh
+++
@@ -1,3 +0,0 @@
-env GOOS=js GOARCH=wasm go build -tags example -o goroutines.wasm -trimpath .
-go run ../../ -wasm goroutines.wasm -namespace Go2DotNet.Example.Goroutines.AutoGen > gen.cs
-dotnet run .
