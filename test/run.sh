44d0f91e27e1e6a016f234c4a789d0473baddf00
Hajime Hoshi <hajimehoshi@gmail.com>
2020-03-14 20:50:02 +0900

    Bug fix: test

--- test/run.sh
+++ test/run.sh
@@ -1,5 +1,5 @@
 echo "# Test $1"
 env GOOS=js GOARCH=wasm go test -c -o test.wasm -trimpath $1
-go run ../gowasm2csharp -wasm test.wasm -namespace Go2DotNet.Test.AutoGen > gen.cs
+go run ../cmd/gowasm2csharp -wasm test.wasm -namespace Go2DotNet.Test.AutoGen > gen.cs
 shift
 dotnet run -c Release . -- $*
