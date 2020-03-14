373e0bbe88f6462372408c800252e05361f2209b
Hajime Hoshi <hajimehoshi@gmail.com>
2020-03-14 18:05:34 +0900

    Use release build

--- test/run.sh
+++ test/run.sh
@@ -2,4 +2,4 @@ echo "# Test $1"
 env GOOS=js GOARCH=wasm go test -c -o test.wasm -trimpath $1
 go run .. -wasm test.wasm -namespace Go2DotNet.Test.AutoGen > gen.cs
 shift
-dotnet run . -- $*
+dotnet run -c Release . -- $*
