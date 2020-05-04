8967ee4625e0534ea2e32f5406cbf1b8fce1294d
Hajime Hoshi <hajimehoshi@gmail.com>
2020-05-05 04:35:02 +0900

    example/helloworld: Add main function

--- example/helloworld/run.sh
+++ example/helloworld/run.sh
@@ -1,5 +1,6 @@
 set -e
 env GOOS=js GOARCH=wasm go build -tags example -o helloworld.wasm -trimpath .
-rm -rf autogen
+rm -rf autogen *.o
 go run ../../cmd/gowasm2cpp -out autogen -wasm helloworld.wasm -namespace go2cpp_autogen
-gcc -c -Wall -std=c++14 -I. autogen/*.cpp
+clang++ -Wall -std=c++14 -I. -o helloworld -v *.cpp autogen/*.cpp
+./helloworld
