01bacda209208cafda1040a0b596f1f76d20649f
Hajime Hoshi <hajimehoshi@gmail.com>
2020-05-05 18:55:58 +0900

    Remove test/binding

--- test/binding/run.sh
+++
@@ -1,5 +0,0 @@
-set -e
-env GOOS=js GOARCH=wasm go test -c -o binding.wasm -trimpath .
-rm -rf autogen
-go run ../../cmd/gowasm2cpp -out autogen -wasm binding.wasm -namespace Go2DotNet.Test.Binding.AutoGen
-dotnet run . -- -test.v
