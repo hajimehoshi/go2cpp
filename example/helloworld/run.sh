e44917ee385788b9b6aed090b5725f93c57bebe3
Hajime Hoshi <hajimehoshi@gmail.com>
2020-05-05 21:04:25 +0900

    Add -pthread

--- example/helloworld/run.sh
+++ example/helloworld/run.sh
@@ -2,5 +2,5 @@ set -e
 env GOOS=js GOARCH=wasm go build -tags example -o helloworld.wasm -trimpath .
 rm -rf autogen *.o
 go run ../../cmd/gowasm2cpp -out autogen -wasm helloworld.wasm -namespace go2cpp_autogen
-clang++ -Wall -std=c++14 -I. -o helloworld -g *.cpp autogen/*.cpp
+clang++ -Wall -std=c++14 -pthread -I. -o helloworld -g *.cpp autogen/*.cpp
 ./helloworld
--- test/stdlib/run.sh
+++ test/stdlib/run.sh
@@ -3,6 +3,6 @@ echo "# Test $1"
 env GOOS=js GOARCH=wasm go test -c -o test.wasm -trimpath $1
 rm -rf autogen
 go run ../../cmd/gowasm2cpp -out autogen -wasm test.wasm -namespace go2cpp_autogen
-clang++ -Wall -std=c++14 -I. -o test -g *.cpp autogen/*.cpp
+clang++ -Wall -std=c++14 -pthread -I. -o test -g *.cpp autogen/*.cpp
 shift
 ./test $*
