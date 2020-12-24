f1f19d81dc3dcc77a1a9daa216d95c5b4b8c1844
Hajime Hoshi <hajimehoshi@gmail.com>
2020-12-24 19:37:34 +0900

    gowasm2cpp: Bug fix: redundant assignment

--- gowasm2cpp/mem.go
+++ gowasm2cpp/mem.go
@@ -178,8 +178,8 @@ Mem::Mem() {
   // Reserving 4GB memory might fail on some consoles. 1GB should be safe in most environments.
   bytes_.reserve(1ul * 1024 * 1024 * 1024);
   bytes_.resize({{.InitPageNum}} * kPageSize);
-{{range $index, $value := .Data}}  std::memcpy(&(bytes_[{{$value.Offset}}]), data_segment_data{{$index}}, {{len $value.Data}});
   bytes_begin_ = &*bytes_.begin();
+{{range $index, $value := .Data}}  std::memcpy(&(bytes_[{{$value.Offset}}]), data_segment_data{{$index}}, {{len $value.Data}});
 {{end}}
 }
 
