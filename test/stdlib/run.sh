d1f094286939e7becdd58cd487f1644d84afbcff
Hajime Hoshi <hajimehoshi@gmail.com>
2020-12-25 21:02:26 +0900

    test/stdlib: Leave a comment how to do optimization (not working yet)

--- test/stdlib/run.sh
+++ test/stdlib/run.sh
@@ -1,6 +1,8 @@
 set -e
 echo "# Test $1"
 env GOOS=js GOARCH=wasm go test -c -o test.wasm -trimpath $1
+# wasm-opt -O2 -g -o test.opt.wasm ./test.wasm
+# mv test.opt.wasm test.wasm
 rm -rf autogen
 go run ../../cmd/gowasm2cpp -out autogen -include autogen -wasm test.wasm -namespace go2cpp_autogen
 clang++ -Wall -std=c++14 -pthread -I. -o test -g *.cpp autogen/*.cpp
