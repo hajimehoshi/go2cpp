c195066ce82599f1a52edae1cc588e3d2d977cf7
Hajime Hoshi <hajimehoshi@gmail.com>
2020-12-27 01:30:15 +0900

    test/stdlib: Remove wasm-opt

--- test/stdlib/run.sh
+++ test/stdlib/run.sh
@@ -1,8 +1,6 @@
 set -e
 echo "# Test $1"
 env GOOS=js GOARCH=wasm go test -c -o test.wasm $1
-# wasm-opt -O2 -g -o test.opt.wasm ./test.wasm
-# mv test.opt.wasm test.wasm
 rm -rf autogen
 go run ../../cmd/gowasm2cpp -out autogen -include autogen -wasm test.wasm -namespace go2cpp_autogen
 clang++ -O3 -Wall -std=c++14 -pthread -I. -o test -g *.cpp autogen/*.cpp
