657714b352b9c79e3da9b36a10c0c95a019ca4ac
Hajime Hoshi <hajimehoshi@gmail.com>
2020-12-27 15:06:57 +0900

    gowasm2cpp: Remove unnecessary compiler directives
    benchstat with -O3
    
    ```
    name       old time/op  new time/op  delta
    SortInt1K   216µs ± 2%   215µs ± 2%   ~     (p=0.690 n=5+5)
    ```

--- gowasm2cpp/mem.go
+++ gowasm2cpp/mem.go
@@ -89,39 +89,39 @@ public:
   int32_t GetSize() const;
   int32_t Grow(int32_t delta);
 
-  inline __attribute__((always_inline)) int8_t LoadInt8(int32_t addr) const {
+  inline int8_t LoadInt8(int32_t addr) const {
     return static_cast<int8_t>(*(bytes_begin_ + addr));
   }
 
-  inline __attribute__((always_inline)) uint8_t LoadUint8(int32_t addr) const {
+  inline uint8_t LoadUint8(int32_t addr) const {
     return *(bytes_begin_ + addr);
   }
 
-  inline __attribute__((always_inline)) int16_t LoadInt16(int32_t addr) const {
+  inline int16_t LoadInt16(int32_t addr) const {
     return *(reinterpret_cast<const int16_t*>(bytes_begin_ + addr));
   }
 
-  inline __attribute__((always_inline)) uint16_t LoadUint16(int32_t addr) const {
+  inline uint16_t LoadUint16(int32_t addr) const {
     return *(reinterpret_cast<const uint16_t*>(bytes_begin_ + addr));
   }
 
-  inline __attribute__((always_inline)) int32_t LoadInt32(int32_t addr) const {
+  inline int32_t LoadInt32(int32_t addr) const {
     return *(reinterpret_cast<const int32_t*>(bytes_begin_ + addr));
   }
 
-  inline __attribute__((always_inline)) uint32_t LoadUint32(int32_t addr) const {
+  inline uint32_t LoadUint32(int32_t addr) const {
     return *(reinterpret_cast<const uint32_t*>(bytes_begin_ + addr));
   }
 
-  inline __attribute__((always_inline)) int64_t LoadInt64(int32_t addr) const {
+  inline int64_t LoadInt64(int32_t addr) const {
     return *(reinterpret_cast<const int64_t*>(bytes_begin_ + addr));
   }
 
-  inline __attribute__((always_inline)) float LoadFloat32(int32_t addr) const {
+  inline float LoadFloat32(int32_t addr) const {
     return *(reinterpret_cast<const float*>(bytes_begin_ + addr));
   }
 
-  inline __attribute__((always_inline)) double LoadFloat64(int32_t addr) const {
+  inline double LoadFloat64(int32_t addr) const {
     return *(reinterpret_cast<const double*>(bytes_begin_ + addr));
   }
 
@@ -129,23 +129,23 @@ public:
     *(bytes_begin_ + addr) = static_cast<uint8_t>(val);
   }
 
-  inline __attribute__((always_inline)) void StoreInt16(int32_t addr, int16_t val) {
+  inline void StoreInt16(int32_t addr, int16_t val) {
     *(reinterpret_cast<int16_t*>(bytes_begin_ + addr)) = val;
   }
 
-  inline __attribute__((always_inline)) void StoreInt32(int32_t addr, int32_t val) {
+  inline void StoreInt32(int32_t addr, int32_t val) {
     *(reinterpret_cast<int32_t*>(bytes_begin_ + addr)) = val;
   }
 
-  inline __attribute__((always_inline)) void StoreInt64(int32_t addr, int64_t val) {
+  inline void StoreInt64(int32_t addr, int64_t val) {
     *(reinterpret_cast<int64_t*>(bytes_begin_ + addr)) = val;
   }
 
-  inline __attribute__((always_inline)) void StoreFloat32(int32_t addr, float val) {
+  inline void StoreFloat32(int32_t addr, float val) {
     *(reinterpret_cast<float*>(bytes_begin_ + addr)) = val;
   }
 
-  inline __attribute__((always_inline)) void StoreFloat64(int32_t addr, double val) {
+  inline void StoreFloat64(int32_t addr, double val) {
     *(reinterpret_cast<double*>(bytes_begin_ + addr)) = val;
   }
 
