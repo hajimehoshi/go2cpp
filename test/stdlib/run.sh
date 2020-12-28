6a413da8235724b9d89b86612ebf677951eb701e
Hajime Hoshi <hajimehoshi@gmail.com>
2020-12-28 16:01:32 +0900

    test/stdlib: Bug fix: Change the directory for tests
    Closes #23

--- test/stdlib/run.sh
+++ test/stdlib/run.sh
@@ -1,8 +1,10 @@
 set -e
-echo "# Test $1"
-env GOOS=js GOARCH=wasm go test -c -o test.wasm $1
+lib=$1
+echo "# Test $lib"
+env GOOS=js GOARCH=wasm go test -c -o test.wasm $lib
 rm -rf autogen
 go run ../../cmd/gowasm2cpp -out autogen -include autogen -wasm test.wasm -namespace go2cpp_autogen
 clang++ -O3 -Wall -std=c++14 -pthread -I. -o test -g *.cpp autogen/*.cpp
 shift
-./test $*
+wd=$PWD
+(cd $(go list -f '{{.Dir}}' $lib) && $wd/test $*)
